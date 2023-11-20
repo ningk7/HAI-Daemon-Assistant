0390bc376e48274cd25228958ad065b80fc3b3cb
comfortablecoffee <csdropout64@gmail.com>
2023-11-19 20:57:26 -0500

    Added variables for full text - the entire body of text and cleaner index storage for quill

--- dist/js/gpt-script.js
+++ dist/js/gpt-script.js
@@ -6,14 +6,24 @@ var quill = new Quill('#editor', {
   });
 
 var highlighted_text; // stores highlighted text
+var end_index_ht; // stores end index of highlighted text
 quill.on('selection-change', function (range, oldRange, source) {
     if (range && range.length > 0) {
         highlighted_text = quill.getText(range.index, range.length);
+        end_index_ht = range.index + range.length;
     } else {
         // No text is currently highlighted
         console.log('No highlighted text');
     }
     });
+
+var fullText = quill.getText(); // stores full text
+var end_index_ft = quill.getLength(); // sotres endIndex of full text
+quill.on('text-change', function(delta, oldDelta, source) {
+  fullText = quill.getText();
+  console.log('Content changed:', fullText);
+  end_index_ft = quill.getLength();
+});
 
   const GPT_call = async(systemPrompt, message)=> {
 
@@ -90,9 +100,14 @@ quill.on('selection-change', function (range, oldRange, source) {
   });
   
   document.querySelector('#elaboratorButton').addEventListener('click', function () {
-    let range = quill.getSelection(true);
     let sys = 'Given a body of text, return a list of up to 5 items (a sentence each at most) that could be further expanded on:';
-    let yourOwnText = call(sys, highlighted_text);
-    // Insert yourOwnText into the Quill editor at the specified range
-    quill.insertText(range.index, '\n' + yourOwnText);
+    
+    if (highlighted_text == null) { // If nothing is highlighted use full script as input
+        console.log(fullText);
+        let yourOwnText = GPT_call(sys, fullText);
+        quill.insertText(end_index_ft, '\n' + yourOwnText);
+    } else { // else use highlighted
+        let yourOwnText = GPT_call(sys, highlighted_text);
+        quill.insertText(end_index_ht, '\n' + yourOwnText);
+    }
     });
