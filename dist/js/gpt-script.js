ed23eb22c6e755524d8ae92402cf78df35c3bced
comfortablecoffee <csdropout64@gmail.com>
2023-11-22 06:07:15 -0500

    Resets highlights on every button press, reformatted prompts and added highlights for elaborate

--- dist/js/gpt-script.js
+++ dist/js/gpt-script.js
@@ -11,7 +11,7 @@ const gptGenerate = async(systemPrompt, message)=> {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${'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'}`,
+          'Authorization': `Bearer ${'2+2 = 4, minus one thats three quick maths'}`,
         },
         body: JSON.stringify({
           model: 'gpt-3.5-turbo',
@@ -50,6 +50,11 @@ quill.on('selection-change', function (range, oldRange, source) {
         console.log('No highlighted text');
     }
     });
+
+function resetHighlight() {
+    let text = quill.getText();
+    quill.formatText(0, text.length, 'background', '#FFFFFF');
+}
 
 async function generateGrammarResponse(text) {
     let user_text = `Text: ${text}
@@ -121,10 +126,12 @@ async function generateSynthesizerResponse(text) {
 }
 
 async function generateElaboratorResponse(text) {
-    let sys = `Given this body of text, return a list of items (up to 5 at max) that could be further expanded followed by a brief reason. ONLY respond in this format: 
-    1. Text: ... Reason: ...
-    2. Text: ... Reason: ...`
-    
+    let sys = `I want you give me writing advice. Given this body of text, return a list of items (up to 5 at max) that could be further expanded followed by a brief reason. Only output the list of entries, each entry should be in the format (text|reason). Make sure that 'text' is a string within the given body of text.
+    Example:
+    Text: I really like cats. Dogs are amazing creatures. They are loyal and fun. They can be fluffly and cute too.
+    Topics:
+    (I really like cats.|You should expand a little bit about cats before talking about dogs here.)`
+
     let user; 
     if (highlighted_text == null) { 
         console.log("elaborating on full text");
@@ -134,12 +141,45 @@ async function generateElaboratorResponse(text) {
         user = `Text: ${highlighted_text}`
     }
     console.log(user);
-    let res = await gptGenerate(sys, user);
+    // Reformatted to match consistency of other functions
+    let res_gpt = await gptGenerate(sys, user);
+    let responseList = res_gpt.split('(');
+    let res = "";
+    let numError = 1;
+
+    responseList.forEach((response) => {
+        // remove closing parenthesis from response
+        response = response.substring(0, response.length - 1);
+        let parsedResponse = response.split('|');
+        if (parsedResponse.length < 2) {
+            console.log("Parsed Response doesn't contain all contents.");
+            return;
+        }
+        let initialSentence = parsedResponse[0];
+        let reason = parsedResponse[1];
+        reason = reason.substring(0, reason.length - 1);
+        if (reason.substring(0, 7) === "Reason:") {
+            reason = reason.substring(7, reason.length)
+        }
+        let sentenceInd = text.search(initialSentence);
+        if (sentenceInd === -1) {
+            console.log("Cannot find initial sentence in input: " + initialSentence);
+            // return;
+        }
+        if (reason.search("No corrections needed") !== -1) {
+            console.log("Reason states that there are no corrections needed for sentence.")
+            return;
+        }
+        quill.formatText(sentenceInd, initialSentence.length, 'background', '#99FF33');
+        res += "[" + numError + "]\nTopic: " + initialSentence + "\nReason: " + reason + "\n--------------------\n";
+        numError += 1;
+    })
     return res;
 }
 
 document.querySelector('#grammarRoverButton').addEventListener('click', async function() {
     setResponse("Loading...");
+    resetHighlight();
     let text = quill.getText();
     let res = await generateGrammarResponse(text);
     if (res === undefined) {
@@ -155,6 +195,7 @@ document.querySelector('#grammarRoverButton').addEventListener('click', async fu
 
 document.querySelector('#synthesizerButton').addEventListener('click', async function() {
     setResponse("Loading...");
+    resetHighlight();
     let text = quill.getText();
     let res = await generateSynthesizerResponse(text); // write GPT calls in this function
     if (res === undefined) {
@@ -166,6 +207,7 @@ document.querySelector('#synthesizerButton').addEventListener('click', async fun
 
 document.querySelector('#elaboratorButton').addEventListener('click', async function() {
     setResponse("Loading...");
+    resetHighlight();
     let text = quill.getText();
     let res = await generateElaboratorResponse(text); // write GPT calls in this function
     if (res === undefined) {
